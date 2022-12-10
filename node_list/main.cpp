42c32dd706ba5ee39e407512fa25f0dc73cfb583
jakob-a2k <jakub.a2k@gmail.com>
2022-12-10 10:45:51 +0100

    delete supplementary struct List{};

--- node_list/main.cpp
+++ node_list/main.cpp
@@ -1,3 +1,5 @@
+
+/*
 #include <iostream>
 
 using namespace std;
@@ -45,3 +47,48 @@ void display(List first_pointer){
         first_pointer.first = first_pointer.first->next;
     }
 }
+*/
+
+#include <iostream>
+
+using namespace std;
+
+struct Node{
+    int value;
+    Node* next;
+};
+void display(Node* first_element);
+
+int main()
+{
+    Node one;
+    Node* first = &one;
+    Node* ptr = first;
+
+    ptr->value = 1;
+    ptr->next = nullptr;
+    display(first);cout << endl;
+
+    Node two;
+    ptr->next = &two;
+    ptr = &two;
+    ptr->value = 2;
+    ptr->next = nullptr;
+    display(first);cout << endl;
+
+    Node three;
+    ptr->next = &three;
+    ptr = &three;
+    ptr->value = 3;
+    ptr->next = nullptr;
+    display(first); cout << endl;
+
+    return 0;
+}
+void display(Node* first_element){
+    while(first_element != 0){
+        cout << first_element->value << endl;
+        first_element = first_element->next;
+    }
+}
+
