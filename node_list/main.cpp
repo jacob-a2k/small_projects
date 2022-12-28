32b2f1d1a1d2cd2bd4339a665a9e416abbffd6e1
jakob-a2k <jakub.a2k@gmail.com>
2022-12-28 11:11:34 +0100

    create - remove_from_choosen_position()

--- node_list/main.cpp
+++ node_list/main.cpp
@@ -21,6 +21,7 @@ bool is_list_empty(const List* const list_ptr);
 void add_back(List* list, Node* new_node);
 Node* remove_back(List* list);
 void add_to_choosen_position(List* list, Node* new_node, int position);
+Node* remove_from_choosen_position(List*  list, int position);
 
 int main()
 {
@@ -66,12 +67,21 @@ void list_methods(List* const list_ptr){
             break;
         case 6:
             exit(0);
-        case 7:
+        case 7:{
             Node* new_node = new Node;
             cout << "Podaj nowa warosc, a nastepnie na ktorym miejscu od gory ma sie znajdowac: ";
             new_node->value = put_value();
             int position = put_value();
             add_to_choosen_position(list_ptr,new_node,position);
+        }
+            break;
+        case 8:
+            if(!is_list_empty(list_ptr)){
+                cout << "Podaj ktory element od gory, ma  zostac usuniety: " << endl;
+                int position = put_value();
+                delete remove_from_choosen_position(list_ptr, position);
+            }
+            break;
         }
     }
 }
@@ -85,6 +95,7 @@ void menu(){
     cout << "5. Display" << endl;
     cout << "6. Exit" << endl;
     cout << "7. Add_in_choosen_location" << endl;
+    cout << "8. Remove from choosen location" << endl;
     cout << endl;
 }
 void display(const List * const list_ptr){
@@ -171,5 +182,22 @@ void add_to_choosen_position(List* list, Node* new_node, int position){
             new_node->next = nullptr;
         else
             new_node->next = tmp;
+    }
+}
+Node* remove_from_choosen_position(List* list, int position){
+    if(position == 1){
+        Node* tmp = list->first;
+        list->first = tmp->next;
+        return tmp;
+    }
+    else{
+        Node* current = list->first;
+        Node* previous = nullptr;
+        for(int i = 1; i < position; i++){
+            previous = current;
+            current = current->next;
+        }
+        previous->next = current->next;
+        return current;
     }
 }
