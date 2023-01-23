ffa0f691f19468e75f1f3498f4707e1d9c698100
jakob-a2k <jakub.a2k@gmail.com>
2023-01-23 11:14:45 +0100

    i can add grandfather but not grandmother

--- genealogical_tree/main.cpp
+++ genealogical_tree/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <limits>
+#include <string.h>
 
 using namespace std;
 
@@ -15,10 +16,11 @@ void tree_options(Tree* tree_ptr);
 void menu();
 int put_number();
 void add_person(Tree* tree_ptr, FamilyMember* person);
-FamilyMember* find_leaf(FamilyMember* current,FamilyMember* wanted);
+FamilyMember* find_leaf(FamilyMember* current,char* wanted);
 FamilyMember* delete_person();
 void change_name();
 void display(FamilyMember* current);
+FamilyMember* new_member(char* c_name);
 
 int main()
 {
@@ -34,13 +36,11 @@ void tree_options(Tree* tree_ptr){
         int number = put_number();
         switch(number){
         case 1:{
-            FamilyMember* person = new FamilyMember;
-            person->name = new char[25];
-            person->father = nullptr;
-            person->mother = nullptr;
-            cout << "Podaj imie i nazwisko dodawanej osoby" << endl;
+            char* full_name = new char[25];
+            cout << "Podaj imie i nazwisko dodawanej osoby!" << endl;
             cin.get();
-            cin.getline(person->name,25);
+            cin.getline(full_name,25);
+            FamilyMember* person = new_member(full_name);
             add_person(tree_ptr,person);
         }
             break;
@@ -67,23 +67,29 @@ void add_person(Tree* tree_ptr, FamilyMember* person){
         tree_ptr->me = person;
     }
     else{
-        FamilyMember* tmp = new FamilyMember;
-        tmp->name = new char[25];
-        tmp->father = nullptr;
-        tmp->mother = nullptr;
-        cout << "Podaj Imie i Nazwisko osoby do ktorej chcesz dodac rodzica!" << endl;
-        cin.getline(tmp->name,25);
-        FamilyMember* current = tree_ptr->me;
-        tmp = find_leaf(current,tmp);
+        FamilyMember* current = nullptr;
+        do{
+            current = tree_ptr->me;
+            char* full_name = new char[25];
+            cout << "Podaj Imie i Nazwisko osoby do ktorej chcesz dodac rodzica!" << endl;
+            cin.getline(full_name,25);
+            current = find_leaf(current,full_name);
+            if(current == 0){
+                cout << "--------------------------------------------------------------" << endl;
+                cout << "Podana osoba nie znajduje sie w spisie drzewa genealogicznego!" << endl;
+                cout << "--------------------------------------------------------------" << endl;
+            }
+        }while(current == 0);
+        cout << "===============" << current << endl;   // sprawdzam czy current zwrocil jakis adress
 
         cout << "Aby dodac mame wpisz 'm',aby dodac ojca wpisz 'f' " << endl;
         char sign;
         cin >> sign;
         if(sign == 'm'){
-            tmp->mother = person;
+            current->mother = new_member(person->name);
         }
         else if(sign == 'f'){
-            tmp->father = person;
+            current->father = new_member(person->name);
         }
     }
 }
@@ -127,15 +133,23 @@ int put_number(){
     }
     return value;
 }
-FamilyMember* find_leaf(FamilyMember* current,FamilyMember* wanted){
-    cout << "///////////" << current->name << "/////////////" << endl;
-    cout << "\\\\\\\\\\\\" << wanted->name << "\\\\\\\\\\\\\\\\\\\\" << endl;
-    if(current->name == wanted->name){
-		return current;
-	}
-	if(current == nullptr){
+FamilyMember* find_leaf(FamilyMember* current,char* wanted){
+    if(current == nullptr){
 		return 0;
+	}
+	cout << "//////////" << current->name << endl; // sprawdzam co aktualnie znajduje sie w current
+	cout << "------------" << wanted <<  endl;     // sprawdzam co aktualnie znajduje sie w wanted
+
+    if(strcmp(current->name, wanted) == 0){
+		return current;
 	}
 	find_leaf(current->mother, wanted);
 	find_leaf(current->father, wanted);
+}
+FamilyMember* new_member(char* c_name){
+    FamilyMember* person = new FamilyMember;
+    person->name = c_name;
+    person->father = nullptr;
+    person->mother = nullptr;
+    return person;
 }
